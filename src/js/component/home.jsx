1197778e2f8103bc64874e1f67b51847491dc969
Shalooko <gonzalo.vergara.t@gmail.com>
2021-09-14 17:56:30 +0000

    todolist

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+//import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+	const [tarea, setTarea] = useState([]);
 
+	const Lista = tarea.map((tarea, index) => {
+		return (
+			<li key={index}>
+				{tarea}
+				<button onClick={() => eliminar(index)}>
+					<i className="fas fa-window-close"></i>
+				</button>
+			</li>
+		);
+	});
+
+	const tecla = e => {
+		if (e.keyCode === 13 && e.target.value !== "") {
+			let texto = tarea.concat(e.target.value);
+			setTarea(texto);
+			e.target.value = "";
+		}
+	};
+
+	const eliminar = i => {
+		let borrar = [...tarea];
+		borrar.splice(i, 1);
+		setTarea(borrar);
+	};
+
+	{
+		return (
+			<div className="container text-center mt-5">
+				<h1>To Do List</h1>
+				<ul className="listItemsClass">
+					<input type="text" onKeyUp={tecla} placeholder="Tareas?" />
+					{Lista}
+				</ul>
+			</div>
+		);
+	}
+};
 export default Home;
